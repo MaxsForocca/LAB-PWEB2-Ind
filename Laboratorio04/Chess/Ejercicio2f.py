fcc9de3283a5da311ab21f635309485c150f4c48
MaxsForocca <mforocca@unsa.edu.pe>
2023-06-05 20:36:49 -0500

    ejercicio 2 f

--- Laboratorio04/Chess/Ejercicio2f.py
+++ Laboratorio04/Chess/Ejercicio2f.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+casillaBN = square.join(square.negative())
+fila = casillaBN.horizontalRepeat(3)
+fila2 = fila.up(fila.negative())
+medioTablero = fila2.up(fila2)
+draw(medioTablero)
