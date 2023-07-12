bc0216272604bfd811b836be51b855e67dbf3b9a
MaxsForocca <mforocca@unsa.edu.pe>
2023-07-11 20:40:49 -0500

    carpeta templates, modificar settings

--- Laboratorio07/Proyecto/Aplicacion1/models.py
+++ Laboratorio07/Proyecto/Aplicacion1/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+"""
+# One to Many
+class Empresa(models.Model):
+    name = models.CharField(max_length = 50)
+    
+    def __str__(self):
+        return self.name
+
+class Videojuego(models.Model):
+    name = models.CharField(max_length = 150)
+    empresa = models.Foreignkey(Empresa, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+"""
+
+"""
+Many to Many
+
+class Movie(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length = 50)
+    movies = models.ManyToManyField(Movie)
+    
+    def __str__(self):
+        return self.name
+"""
