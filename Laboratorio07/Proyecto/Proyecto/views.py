3f0f5744d6e2fd791efe26b11fce0729604d3e27
MaxsForocca <mforocca@unsa.edu.pe>
2023-07-13 10:26:41 -0500

    complete templates to pedf in django

--- Laboratorio07/Proyecto/Proyecto/views.py
+++ Laboratorio07/Proyecto/Proyecto/views.py
@@ -16,9 +16,16 @@ class GeneratePDF(View):
         html = template.render(context)
         pdf = render_to_pdf('invoice.html', context)
         if pdf:
-            return HttpResponse(pdf, content_type = ' application/pdf')
+            response = HttpResponse(pdf, content_type = 'application/pdf')
+            filename = "Invoice_%s.pdf" %("321123")
+            content = "inline; filename='%s'" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" %(filename)
+            response['Content-Disposition'] = content
+            return response
         return HttpResponse("No Encontrado")
-    # videotime = 10:25
+    
 
 """
 def generate_view(request, *args, **kwargs):
